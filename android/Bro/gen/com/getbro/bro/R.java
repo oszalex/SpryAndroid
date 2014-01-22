532a894c3dcd30cbdfde1285f56fe01ad9ae9c69
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-01-22 22:14:38 +0100

    activity added

--- android/Bro/gen/com/getbro/bro/R.java
+++ android/Bro/gen/com/getbro/bro/R.java
@@ -8,6 +8,9 @@
 package com.getbro.bro;
 
 public final class R {
+    public static final class array {
+        public static final int categories=0x7f060000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -22,23 +25,34 @@ public final class R {
     }
     public static final class drawable {
         public static final int gradient=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int logo=0x7f020002;
-        public static final int logo_400x400px=0x7f020003;
+        public static final int head_icon=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int logo=0x7f020003;
+        public static final int logo_400x400px=0x7f020004;
+        public static final int sample_map=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int edit_password=0x7f080002;
-        public static final int edit_username=0x7f080001;
-        public static final int login_logo=0x7f080000;
+        public static final int action_settings=0x7f09000c;
+        public static final int edit_password=0x7f09000b;
+        public static final int edit_username=0x7f09000a;
+        public static final int login_logo=0x7f090009;
+        public static final int map=0x7f090001;
+        public static final int profiles=0x7f090002;
+        public static final int profilimage1=0x7f090003;
+        public static final int profilimage2=0x7f090004;
+        public static final int profilimage3=0x7f090005;
+        public static final int profilimage4=0x7f090006;
+        public static final int profilimage5=0x7f090007;
+        public static final int spinner1=0x7f090000;
+        public static final int time=0x7f090008;
     }
     public static final class layout {
         public static final int activity_input=0x7f030000;
         public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int input=0x7f070000;
-        public static final int main=0x7f070001;
+        public static final int input=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -48,6 +62,8 @@ public final class R {
         public static final int edit_username=0x7f050002;
         public static final int hello_world=0x7f050007;
         public static final int logo_description=0x7f050005;
+        public static final int more=0x7f050009;
+        public static final int std_category=0x7f050008;
         /**  Input Activity 
          */
         public static final int title_activity_input=0x7f050006;
@@ -74,10 +90,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
