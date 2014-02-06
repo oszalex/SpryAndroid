1832ac544e18cb255d277e03f6c0ac77ef37871e
Dominik Pichler <dpichler90@gmail.com>
2014-02-06 23:02:00 +0100

    Activity showFriend added. Changed look of Input Activity just for testing-reason

--- android/Bro/gen/com/getbro/bro/R.java
+++ android/Bro/gen/com/getbro/bro/R.java
@@ -9,9 +9,12 @@ package com.getbro.bro;
 
 public final class R {
     public static final class array {
-        public static final int categories=0x7f060000;
+        public static final int categories=0x7f070000;
     }
     public static final class attr {
+    }
+    public static final class color {
+        public static final int textColor=0x7f040000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -20,53 +23,61 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int gradient=0x7f020000;
-        public static final int head_icon=0x7f020001;
-        public static final int ic_launcher=0x7f020002;
-        public static final int logo=0x7f020003;
-        public static final int logo_400x400px=0x7f020004;
-        public static final int sample_map=0x7f020005;
+        public static final int buddy=0x7f020000;
+        public static final int gradient=0x7f020001;
+        public static final int head_icon=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int logo=0x7f020004;
+        public static final int logo_400x400px=0x7f020005;
+        public static final int sample_map=0x7f020006;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000c;
-        public static final int edit_password=0x7f09000b;
-        public static final int edit_username=0x7f09000a;
-        public static final int login_logo=0x7f090009;
-        public static final int map=0x7f090002;
-        public static final int profiles=0x7f090003;
-        public static final int profilimage1=0x7f090004;
-        public static final int profilimage2=0x7f090005;
-        public static final int profilimage3=0x7f090006;
-        public static final int profilimage4=0x7f090007;
-        public static final int profilimage5=0x7f090008;
-        public static final int spinner1=0x7f090000;
-        public static final int time=0x7f090001;
+        public static final int action_settings=0x7f0a0010;
+        public static final int buttons=0x7f0a0009;
+        public static final int edit_password=0x7f0a000c;
+        public static final int edit_username=0x7f0a000b;
+        public static final int friend_events=0x7f0a000f;
+        public static final int friend_sex=0x7f0a000e;
+        public static final int friend_username=0x7f0a000d;
+        public static final int login_logo=0x7f0a000a;
+        public static final int map=0x7f0a0002;
+        public static final int profiles=0x7f0a0003;
+        public static final int profilimage1=0x7f0a0004;
+        public static final int profilimage2=0x7f0a0005;
+        public static final int profilimage3=0x7f0a0006;
+        public static final int profilimage4=0x7f0a0007;
+        public static final int profilimage5=0x7f0a0008;
+        public static final int spinner1=0x7f0a0000;
+        public static final int time=0x7f0a0001;
     }
     public static final class layout {
         public static final int activity_input=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int activity_show_friend=0x7f030002;
     }
     public static final class menu {
-        public static final int input=0x7f080000;
-        public static final int main=0x7f080001;
+        public static final int input=0x7f090000;
+        public static final int main=0x7f090001;
+        public static final int show_friend=0x7f090002;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int button_login=0x7f050004;
-        public static final int edit_password=0x7f050003;
-        public static final int edit_username=0x7f050002;
-        public static final int hello_world=0x7f050007;
-        public static final int logo_description=0x7f050005;
-        public static final int more=0x7f050009;
-        public static final int std_category=0x7f050008;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int button_login=0x7f060004;
+        public static final int edit_password=0x7f060003;
+        public static final int edit_username=0x7f060002;
+        public static final int hello_world=0x7f060007;
+        public static final int logo_description=0x7f060005;
+        public static final int more=0x7f060009;
+        public static final int std_category=0x7f060008;
         /**  Input Activity 
          */
-        public static final int title_activity_input=0x7f050006;
+        public static final int title_activity_input=0x7f060006;
+        public static final int title_activity_show_friend=0x7f06000a;
     }
     public static final class style {
         /** 
@@ -90,10 +101,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f070001;
+        public static final int AppTheme=0x7f080001;
     }
 }
