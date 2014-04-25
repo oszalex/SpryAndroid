719a2606aa70db5cd2443a2593f01af743f9f8fa
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-04-25 18:53:08 +0200

    changed dev server to http

--- server/server.py
+++ server/server.py
@@ -2,11 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from broapp import app
-from OpenSSL import SSL
+#from OpenSSL import SSL
 
-ctx = SSL.Context(SSL.SSLv23_METHOD)
-ctx.use_privatekey_file('cert/ssl.key')
-ctx.use_certificate_file('cert/ssl.cert')
+#ctx = SSL.Context(SSL.SSLv23_METHOD)
+#ctx.use_privatekey_file('cert/ssl.key')
+#ctx.use_certificate_file('cert/ssl.cert')
 
-app.run(host='0.0.0.0',port=8080,
-	debug = True, ssl_context=ctx)
+app.run(host='0.0.0.0',port=8080, debug = True)
