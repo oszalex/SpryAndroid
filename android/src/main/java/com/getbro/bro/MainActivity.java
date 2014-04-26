938196342c49d31ba056c953d875b1a94f57bb18
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-04-26 15:47:36 +0200

    fixed #51

--- android/src/main/java/com/getbro/bro/MainActivity.java
+++ android/src/main/java/com/getbro/bro/MainActivity.java
@@ -1,5 +1,7 @@
 package com.getbro.bro;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -7,19 +9,38 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import com.getbro.bro.EventParser.TokenWebserviceResource;
 import com.getbro.bro.Webservice.HttpGetRequest;
 import org.apache.http.client.methods.HttpGet;
+import android.support.v4.widget.DrawerLayout;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
 
 	public final static String EXTRA_USERNAME = "com.getbro.bro.USERNAME";
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+        /** init sidebar **/
+        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.drawer_list_item, mPlanetTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
 	}
 
 	@Override
@@ -58,4 +79,30 @@ public class MainActivity extends Activity {
 //		startActivity(intent);
 //
 	}
+
+
+    public class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            selectItem(position);
+        }
+    }
+
+
+    /** Swaps fragments in the main content view */
+    private void selectItem(int position) {
+        /*
+
+
+         */
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        /*
+        mTitle = title;
+        getActionBar().setTitle(mTitle);
+         */
+
+    }
 }
