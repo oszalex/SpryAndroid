ea53d0c2df67b7ff5a6f7f36d9bb413848cfea64
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-04-27 11:50:54 +0200

    refactored: moved login specific thing to the correspondending activity

--- android/src/main/java/com/getbro/bro/LoginActivity.java
+++ android/src/main/java/com/getbro/bro/LoginActivity.java
@@ -33,4 +33,38 @@ public class LoginActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+    /**
+     * old login method
+     *
+     * public void login(View view) {
+     EditText mUsername = (EditText)findViewById(R.id.login_username);
+     EditText mPassword = (EditText)findViewById(R.id.login_password);
+
+     String username = mUsername.getText().toString();
+     String password = mPassword.getText().toString();
+
+     HttpGetRequest httpRequest = (HttpGetRequest)getApplication();
+     //  httpRequest.configureClient(getResources().getString(R.string.webService),username,password);
+
+     Intent intent = new Intent(this,ProfilActivity.class);
+     startActivity(intent);
+
+     //
+     //		 TODO verify login and throw alert if no valid
+     //
+    //
+    //		//Intent provides runtime bindings between components.
+    //		Intent intent = new Intent(this, InputActivity.class);
+    //
+    //		// Pass the username as an extra. To be displayed in the Input-Activity
+    //		EditText edit_username = (EditText) findViewById(R.id.edit_username);
+    //		String username = edit_username.getText().toString();
+    //		intent.putExtra(EXTRA_USERNAME, username);
+    //
+    //		startActivity(intent);
+    //
+    }
+     */
 }
--- android/src/main/java/com/getbro/bro/MainActivity.java
+++ android/src/main/java/com/getbro/bro/MainActivity.java
@@ -1,20 +1,11 @@
 package com.getbro.bro;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.os.Bundle;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import com.getbro.bro.EventParser.TokenWebserviceResource;
-import com.getbro.bro.Webservice.HttpGetRequest;
-import org.apache.http.client.methods.HttpGet;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ListView;
 
@@ -50,35 +41,9 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+
 	/** Called when the user clicks the login button */
-	public void login(View view) {
-        EditText mUsername = (EditText)findViewById(R.id.login_username);
-        EditText mPassword = (EditText)findViewById(R.id.login_password);
 
-        String username = mUsername.getText().toString();
-        String password = mPassword.getText().toString();
-
-        HttpGetRequest httpRequest = (HttpGetRequest)getApplication();
-      //  httpRequest.configureClient(getResources().getString(R.string.webService),username,password);
-
-        Intent intent = new Intent(this,ProfilActivity.class);
-        startActivity(intent);
-
-//		/**
-//		 * TODO verify login and throw alert if no valid
-//		 */
-//
-//		/** Intent provides runtime bindings between components. */
-//		Intent intent = new Intent(this, InputActivity.class);
-//
-//		/** Pass the username as an extra. To be displayed in the Input-Activity */
-//		EditText edit_username = (EditText) findViewById(R.id.edit_username);
-//		String username = edit_username.getText().toString();
-//		intent.putExtra(EXTRA_USERNAME, username);
-//
-//		startActivity(intent);
-//
-	}
 
 
     public class DrawerItemClickListener implements ListView.OnItemClickListener {
