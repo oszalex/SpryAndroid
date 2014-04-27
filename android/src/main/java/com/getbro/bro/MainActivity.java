9aa7ff8f1187fd4b9a94f7dd3f05de7a1d67f7f2
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-04-27 12:45:34 +0200

    navigation added fixed #52

--- android/src/main/java/com/getbro/bro/MainActivity.java
+++ android/src/main/java/com/getbro/bro/MainActivity.java
@@ -1,17 +1,20 @@
 package com.getbro.bro;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.ListView;
 
-public class MainActivity extends Activity {
 
-	public final static String EXTRA_USERNAME = "com.getbro.bro.USERNAME";
+public class MainActivity extends Activity {
     private String[] mPlanetTitles;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
@@ -22,7 +25,7 @@ public class MainActivity extends Activity {
 		setContentView(R.layout.activity_main);
 
         /** init sidebar **/
-        mPlanetTitles = getResources().getStringArray(R.array.planets_array);
+        mPlanetTitles = getResources().getStringArray(R.array.drawer_items);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
 
@@ -32,6 +35,9 @@ public class MainActivity extends Activity {
         // Set the list's click listener
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
 
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+        selectItem(2);
 	}
 
 	@Override
@@ -42,10 +48,6 @@ public class MainActivity extends Activity {
 	}
 
 
-	/** Called when the user clicks the login button */
-
-
-
     public class DrawerItemClickListener implements ListView.OnItemClickListener {
         @Override
         public void onItemClick(AdapterView parent, View view, int position, long id) {
@@ -54,20 +56,88 @@ public class MainActivity extends Activity {
     }
 
 
-    /** Swaps fragments in the main content view */
     private void selectItem(int position) {
-        /*
+        // update the main content by replacing fragments
 
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(
+                R.id.content_frame,
+                getFragment(position)
+        ).commit();
 
-         */
+        // update selected item and title, then close the drawer
+        mDrawerList.setItemChecked(position, true);
+        setTitle(mPlanetTitles[position]);
+        mDrawerLayout.closeDrawer(mDrawerList);
     }
+
+    private Fragment getFragment(int i){
+        switch(i){
+            case 0:
+                return new ProfilFragment();
+            case 1:
+                return new FriendListFragment();
+            case 2:
+                return new EventListFragment();
+            case 3:
+                return new NewEventFragment();
+            default:
+                return new EventListFragment();
+        }
+    }
+
 
     @Override
     public void setTitle(CharSequence title) {
-        /*
-        mTitle = title;
-        getActionBar().setTitle(mTitle);
-         */
+        getActionBar().setTitle(title);
+    }
 
+
+    public class ProfilFragment extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+
+            return inflater.inflate(R.layout.activity_profil, container, false);
+
+        }
+    }
+
+    public class EventListFragment extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+
+            return inflater.inflate(R.layout.activity_display_events, container, false);
+
+        }
+    }
+
+    public class NewEventFragment extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+
+            return inflater.inflate(R.layout.activity_newevent, container, false);
+
+        }
+    }
+
+    public class FriendListFragment extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+
+            return inflater.inflate(R.layout.activity_friendlist, container, false);
+
+        }
     }
 }
