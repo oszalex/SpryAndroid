8d02cb69c15fb16c6ca2e24356ef63ad01632991
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-05-03 21:09:30 +0200

    typofix

--- server/broapp/controller/autocomplete.py
+++ server/broapp/controller/autocomplete.py
@@ -26,7 +26,7 @@ autocomplete = Blueprint('autocomplete', __name__)
 def get_regex_user(regex, page_num):
     ''' find users with a name like <regex> '''
     query = db.session.query(User).filter(User.username.like(regex + "%"))
-    items = qery.paginate(page_num, USERS_PER_RESONSE).items
+    items = query.paginate(page_num, USERS_PER_RESONSE).items
     return jsonify({"data": UserSerializer(items, many=True).data })
 
 
