4f861bbb32ad82f5a58bd48bba5a1eaa9ab768c5
Christian Hotz-Behofsits <chris.hotz.behofsits@gmail.com>
2014-05-06 10:14:31 +0200

    catch nullpointer excetption on update

--- android/src/main/java/com/getbro/bro/Webservice/DatabaseSync.java
+++ android/src/main/java/com/getbro/bro/Webservice/DatabaseSync.java
@@ -7,36 +7,49 @@ import com.getbro.bro.Json.Event;
 import com.getbro.bro.Json.User;
 import com.getbro.bro.Model.EventModel;
 import com.getbro.bro.Model.UserModel;
+import com.orm.Database;
+
+import static junit.framework.Assert.assertNotNull;
 
 /**
  * Created by chris on 04/05/14.
  */
 
 public class DatabaseSync extends AsyncTask<Void, Void, Void> {
+    private final String TAG = DatabaseSync.class.getSimpleName();
     private AsyncLoginResponse delegate = null;
     private HttpGetRequest httpRequest;
 
     public DatabaseSync(AsyncLoginResponse callback) {
-        Log.d("Login", "new LoginCheck");
+        Log.d(TAG, "sync database");
 
         this.delegate = callback;
         httpRequest = callback.getHTTPRequest();
+
+        assertNotNull("httpRequest cannot be null", httpRequest);
     }
 
 
     @Override
     protected Void doInBackground(Void... params) {
-        Log.d("Login", "some background work");
-
         //fetch events
-        Event events[] = httpRequest.getAllEvents();
+        try {
+            Event events[] = httpRequest.getAllEvents();
 
-        for(Event e: events)
-            EventModel.updateOrInsert(delegate.getApplicationContext(), e);
+            for (Event e : events)
+                EventModel.updateOrInsert(delegate.getApplicationContext(), e);
+
+        } catch (NullPointerException e){
+            Log.w(TAG, "cannot update event list! (NullPointer)");
+        }
 
         //fetch own user object (users/me)
-        User me = httpRequest.getOwnUserElement();
-        UserModel.updateOrInsert(delegate.getApplicationContext(), me);
+        try {
+            User me = httpRequest.getOwnUserElement();
+            UserModel.updateOrInsert(delegate.getApplicationContext(), me);
+        } catch(NullPointerException e){
+            Log.w(TAG, "cannot update current user information! (NullPointer)");
+        }
 
         //fetch followers/followings
 
@@ -45,7 +58,7 @@ public class DatabaseSync extends AsyncTask<Void, Void, Void> {
     }
 
     protected void onPostExecute(Void result) {
-        Log.d("Login", "Resultat ist da!");
+        Log.d(TAG, "Resultat ist da!");
 
         delegate.onLoginCheckFinish(true);
     }
